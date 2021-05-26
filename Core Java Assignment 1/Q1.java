0f0d12ccf3646640b5bdb5d01864e19c2d3692e7
Shreyash Amberkhane <83824115+shreyash3105@users.noreply.github.com>
2021-05-26 10:30:33 +0530

    Update Q1.java

--- Core Java Assignment 1/Q1.java
+++ Core Java Assignment 1/Q1.java
@@ -1,6 +1,7 @@
 /*01.	Write a program to print Hello World. Compile and run it using command prompt.*/
+//This Program has to be run in cmd. After succesfull compilation, while execution, add the word "World" after class name.
 class Q1{
 	public static void main(String [] args){
-		System.out.println("Hello World");
+		System.out.println("Hello " + args[0]);
 	}
 }
