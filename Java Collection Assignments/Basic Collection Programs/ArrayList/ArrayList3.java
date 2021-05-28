fe3a76948802d2e1d7ca1e388977c91af1ae2fb8
Shreyash Amberkhane <83824115+shreyash3105@users.noreply.github.com>
2021-05-28 15:18:06 +0530

    Update ArrayList3.java

--- Java Collection Assignments/Basic Collection Programs/ArrayList/ArrayList3.java
+++ Java Collection Assignments/Basic Collection Programs/ArrayList/ArrayList3.java
@@ -8,7 +8,7 @@ public class ArrayList3 {
         list.add("Third");
         list.add("Fourth");
         list.add(0, "Fifth"); //adding element at first position
-        list.add(3, "Sixth"); //adding element at third position2
+        list.add(3, "Sixth"); //adding element at third position
         //list.add(index, element);
         for(int i =0; i<list.size(); i++){
             System.out.println(list.get(i));
