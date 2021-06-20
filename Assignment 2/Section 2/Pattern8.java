656007c1ebdf4da22ba0cbdacf6f561c4b00ef66
Shreyash Amberkhane <83824115+shreyash3105@users.noreply.github.com>
2021-06-20 21:05:41 +0530

    Add files via upload

---
+++ Assignment 2/Section 2/Pattern8.java
@@ -0,0 +1,15 @@
+class Pat5
+{
+public static void main(String[] args)
+{
+int x=64;
+for(int i =1;i<=5;i++)
+{
+for (int j =1;j<=i;j++)
+{
+System.out.print((char)(x+i)  +" ");
+}
+System.out.println();
+}
+}
+}
