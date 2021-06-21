e2af5831eed3c31a6cf1d289fade3ceff015797e
Shreyash Amberkhane <83824115+shreyash3105@users.noreply.github.com>
2021-06-21 20:28:33 +0530

    Add files via upload

---
+++ Assignment 2/Section 2/Pattern11.java
@@ -0,0 +1,15 @@
+class Pattern11
+{
+	public static void main(String args[])
+	{	
+		for(int i=1;i<=5;i++){
+			for(int j=2;j<=i;j++){
+				System.out.print(" ");
+				}
+			 for(int k=5;k>=1;k--){
+				System.out.print("*");
+				}
+			System.out.println();			 
+			}
+	}
+}
